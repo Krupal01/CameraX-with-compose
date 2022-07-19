1a4028c37ec953ba0081304c471fcae97c8f6913
krupal <bhuvakrupal01@gmail.com>
2022-07-19 13:42:09 +0530

    ADD : CameraX Video Recording

--- app/src/main/java/com/example/camerax_compose/MainActivity.kt
+++ app/src/main/java/com/example/camerax_compose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.camerax_compose
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -8,26 +9,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.zIndex
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -36,7 +33,15 @@ import com.example.camerax_compose.composable.CameraView
 import com.example.camerax_compose.composable.OnPermissionResult
 import com.example.camerax_compose.ui.theme.CameraX_ComposeTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
 import java.util.concurrent.ExecutorService
@@ -48,9 +53,13 @@ class MainActivity : ComponentActivity() {
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var photoUri: Uri
+    private lateinit var videoUri: Uri
     private var shouldShowPhoto: MutableState<Boolean> = mutableStateOf(false)
+    private var shouldShowVideo : MutableState<Boolean> = mutableStateOf(false)
     private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+    private var shouldAudioRecord : MutableState<Boolean> = mutableStateOf(false)
 
+    @SuppressLint("RememberReturnType")
     @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,14 +67,14 @@ class MainActivity : ComponentActivity() {
         outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
         setContent {
-            val permissionState = rememberPermissionState(
-                permission = Manifest.permission.CAMERA
+            val permissionState = rememberMultiplePermissionsState(
+                permissions = listOf(Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO)
             )
             val lifecycleOwner = LocalLifecycleOwner.current
             DisposableEffect(key1 = lifecycleOwner, effect = {
                 val observer = LifecycleEventObserver{ _, evet ->
                     if(evet == Lifecycle.Event.ON_START){
-                        permissionState.launchPermissionRequest()
+                        permissionState.launchMultiplePermissionRequest()
                     }
                 }
                 lifecycleOwner.lifecycle.addObserver(observer)
@@ -81,33 +90,68 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val context = LocalContext.current
+                    permissionState.permissions.forEach {
+                        when(it.permission){
+                            Manifest.permission.CAMERA->{
+                                OnPermissionResult(
+                                    status = it.status,
+                                    isGranted = { shouldShowCamera.value = true },
+                                    shouldShowRationale = {Text(text = "Allow Camera permission")},
+                                    isPermanentlyDenied = {Text(text = " Camera Permission Permanently Denied , Allow it from Settings")}
+                                )
+                            }
+                            Manifest.permission.RECORD_AUDIO->{
+                                OnPermissionResult(
+                                    status = it.status,
+                                    isGranted = { shouldAudioRecord.value = true },
+                                    shouldShowRationale = {Text(text = "Allow Audio Record permission")},
+                                    isPermanentlyDenied = {Text(text = " Audio Record Permission Permanently Denied , Allow it from Settings")}
+                                )
+                            }
+                        }
+                    }
 
-                    OnPermissionResult(
-                        status = permissionState.status,
-                        isGranted = {
-                            shouldShowCamera.value = true
-                        },
-                        shouldShowRationale = {Text(text = "Allow Camera permission")},
-                        isPermanentlyDenied = {Text(text = " Camera Permission Permanently Denied , Allow it from Settings")}
-                    )
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd){
-                        if (shouldShowCamera.value) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter){
+                        if (shouldShowCamera.value && shouldAudioRecord.value) {
                             CameraView(
                                 outputDirectory = outputDirectory,
                                 executor = cameraExecutor,
                                 onImageCaptured = ::handleImageCapture,
-                                onError = { Log.e("TAG", "View error:", it) }
+                                onError = { Log.e("TAG", "View error:", it) },
+                                onVideoRecord = ::handleVideoRecording
                             )
                         }
                         if (shouldShowPhoto.value) {
                             Image(
                                 painter = rememberAsyncImagePainter(photoUri),
                                 contentDescription = null,
-                                modifier = Modifier.padding(end = 50.dp, bottom = 20.dp).size(100.dp)
-                                    .border(width = 2.dp, color = Color.White, shape = RectangleShape)
+                                modifier = Modifier
+                                    .padding(bottom = 130.dp)
+                                    .size(150.dp)
+                                    .border(
+                                        width = 2.dp,
+                                        color = Color.White,
+                                        shape = RectangleShape
+                                    )
                                     .zIndex(2f),
                                 contentScale = ContentScale.FillBounds
                             )
+                        }
+                        if (shouldShowVideo.value){
+                            val exoPlayer = remember(key1 = context) {
+                                ExoPlayer.Builder(context).build().apply {
+                                    val dataSourceFactory = DefaultDataSourceFactory(context,Util.getUserAgent(context,context.packageName))
+                                    val source = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(videoUri))
+                                    prepare(source)
+                                }
+                            }
+
+                            AndroidView(factory = { mContext ->
+                                PlayerView(mContext).apply {
+                                    player = exoPlayer
+                                }
+                            }, modifier = Modifier.size(500.dp).padding(bottom = 130.dp))
                         }
                     }
                 }
@@ -121,6 +165,12 @@ class MainActivity : ComponentActivity() {
         shouldShowCamera.value = false
         photoUri = uri
         shouldShowPhoto.value = true
+    }
+
+    private fun handleVideoRecording(uri: Uri){
+        Log.i("TAG","Video Uri: $uri")
+        videoUri = uri
+        shouldShowVideo.value = true
     }
 
     private fun getOutputDirectory(): File {
--- app/src/main/java/com/example/camerax_compose/composable/CameraView.kt
+++ app/src/main/java/com/example/camerax_compose/composable/CameraView.kt
@@ -1,27 +1,23 @@
 package com.example.camerax_compose.composable
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.Recording
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Lens
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,6 +25,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.File
 import java.text.SimpleDateFormat
@@ -37,14 +34,22 @@ import java.util.concurrent.Executor
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+private lateinit var videoCapture: VideoCapture
+
 @SuppressLint("RestrictedApi")
 @Composable
 fun CameraView(
     outputDirectory: File,
     executor: Executor,
     onImageCaptured: (Uri) -> Unit,
-    onError: (ImageCaptureException) -> Unit
+    onError: (ImageCaptureException) -> Unit,
+    onVideoRecord : (Uri)->Unit
 ) {
+
+    var isCapturing by remember {
+        mutableStateOf(false)
+    }
+
     val lensFacing = CameraSelector.LENS_FACING_BACK
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
@@ -53,19 +58,27 @@ fun CameraView(
     val previewView = remember { PreviewView(context) }
 
     val imageCapture: ImageCapture = remember { ImageCapture.Builder().build() }
+//    var videoCapture: VideoCapture = remember { VideoCapture.Builder().build() }
 
     val cameraSelector = CameraSelector.Builder()
         .requireLensFacing(lensFacing)
         .build()
 
     LaunchedEffect(lensFacing) {
+
+        videoCapture = VideoCapture.Builder().apply {
+            setTargetRotation(previewView.display.rotation)
+            setCameraSelector(cameraSelector)
+        }.build()
+
         val cameraProvider = context.getCameraProvider()
         cameraProvider.unbindAll()
         cameraProvider.bindToLifecycle(
             lifecycleOwner,
             cameraSelector,
             preview,
-            imageCapture
+//            imageCapture,
+            videoCapture
         )
 
         preview.setSurfaceProvider(previewView.surfaceProvider)
@@ -74,34 +87,97 @@ fun CameraView(
     Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
         AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
 
-        IconButton(
-            modifier = Modifier.padding(bottom = 20.dp),
-            onClick = {
-                Log.i("TAG", "ON CLICK")
-                takePhoto(
-                    filenameFormat = "yyyy-MM-dd-HH-mm-ss-SSS",
-                    imageCapture = imageCapture,
-                    outputDirectory = outputDirectory,
-                    executor = executor,
-                    onImageCaptured = onImageCaptured,
-                    onError = onError
-                )
-            },
-            content = {
-                Icon(
-                    imageVector = Icons.Sharp.Lens,
-                    contentDescription = "Take picture",
-                    tint = Color.White,
-                    modifier = Modifier
-                        .size(100.dp)
-                        .padding(1.dp)
-                        .border(1.dp, Color.White, CircleShape)
-                )
-            }
-        )
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            IconButton(
+                modifier = Modifier.padding(bottom = 20.dp),
+                onClick = {
+                    takePhoto(
+                        filenameFormat = "yyyy-MM-dd-HH-mm-ss-SSS",
+                        imageCapture = imageCapture,
+                        outputDirectory = outputDirectory,
+                        executor = executor,
+                        onImageCaptured = onImageCaptured,
+                        onError = onError
+                    )
+                },
+                content = {
+                    Icon(
+                        imageVector = Icons.Sharp.Lens,
+                        contentDescription = "Take picture",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .padding(1.dp)
+                            .border(1.dp, Color.White, CircleShape)
+                    )
+                }
+            )
+
+            IconButton(
+                modifier = Modifier.padding(bottom = 20.dp),
+                onClick = {
+                    if (isCapturing){
+                        isCapturing = false
+                        videoCapture.stopRecording()
+                    }else{
+                        isCapturing = true
+                        captureVideo(
+                            filenameFormat = "yyyy-MM-dd-HH-mm-ss-SSS",
+                            videoCapture= videoCapture,
+                            outputDirectory = outputDirectory,
+                            executor = executor,
+                            context = context,
+                            onVideoRecord = onVideoRecord
+                        )
+                    }
+                },
+                content = {
+                    Icon(
+                        imageVector = Icons.Sharp.Lens,
+                        contentDescription = "Video Capture",
+                        tint = if (isCapturing) Color.Red else Color.White,
+                        modifier = Modifier
+                            .size(100.dp)
+                            .padding(1.dp)
+                            .border(1.dp, Color.White, CircleShape)
+                    )
+                }
+            )
+        }
     }
 }
 
+@SuppressLint("RestrictedApi")
+private fun captureVideo(
+    filenameFormat: String,
+    videoCapture: VideoCapture,
+    outputDirectory: File,
+    executor: Executor,
+    context: Context,
+    onVideoRecord : (Uri)->Unit
+){
+
+    val videoFile = File(
+        outputDirectory,
+        SimpleDateFormat(filenameFormat, Locale.US).format(System.currentTimeMillis())+".mp4"
+    )
+
+    val outputOptions = VideoCapture.OutputFileOptions.Builder(videoFile).build()
+
+    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+        videoCapture.startRecording(outputOptions,executor,object : VideoCapture.OnVideoSavedCallback{
+            override fun onVideoSaved(outputFileResults: VideoCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(videoFile)
+                onVideoRecord(savedUri)
+            }
+
+            override fun onError(videoCaptureError: Int, message: String, cause: Throwable?) {
+                Log.i("TAG", "Take Video Error $message $cause")
+//                onError(cause = cause , message = message , videoCaptureError = videoCaptureError)
+            }
+        })
+    }
+}
 
 
 private fun takePhoto(
